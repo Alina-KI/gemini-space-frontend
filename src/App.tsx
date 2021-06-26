bbe0872687911236cedb9cd3543383d13dcfac98
Alina-KI <kraynova.alina.2002@mail.ru>
2021-06-26 22:18:27 +0300

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import s from './app.module.scss'
+import { BrowserRouter } from 'react-router-dom'
+import { Header } from './components/header/header'
+import { Navbar } from './components/navbar/navbar'
+import { Body } from './components/body/body'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <div className={s.container}>
+          <Navbar/>
+          <Body/>
+        </div>
+        <div className={s.footer}>
+          <div className={s.containerFooter}>
+            Footer
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 }
-
-export default App;
