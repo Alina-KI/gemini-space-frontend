18ee219611bf84d0c6099be4564a55fb55dff8fa
Alina-KI <kraynova.alina.2002@mail.ru>
2021-09-01 16:40:54 +0300

    started gallery

---
+++ src/components/pages/gallery/gallery.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import s from './gallery.module.scss'
+
+export const Gallery = () => {
+  // const images = ['http://localhost:300/images/1.jpg','../../../images/18.jpg',
+  //   '../../../images/2.jpg','../../../images/11.jpg',
+  //   '../../../images/3.jpg','../../../images/10.jpg',
+  //   '../../../images/4.jpg','../../../images/9.jpg',
+  //   '../../../images/5.jpg','../../../images/8.jpg',
+  //   '../../../images/12.jpg','../../../images/15.jpg',
+  //   '../../../images/16.jpg','../../../images/17.jpg',
+  //   '../../../images/13.jpg','../../../images/14.jpg',
+  //   '../../../images/6.jpg','../../../images/7.jpg']
+  return (
+    <div className={s.container}>
+      {/*{images.map(value => {*/}
+      {/*  console.log(value)*/}
+      {/*  return(*/}
+      {/*    <div className={s.content}>*/}
+      {/*      <div className={s.image} style={{ backgroundImage: `url(${value})` }}/>*/}
+      {/*      <div className={s.image}> </div>*/}
+      {/*    </div>*/}
+      {/*  )*/}
+      {/*})}*/}
+    </div>
+  )
+}
+
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,16 +5,19 @@ import { MyPage } from './components/pages/my-page/my-page'
 import { Message } from './components/pages/message/message'
 import { AuthPage } from './components/pages/auth/auth'
 import { Registration } from './components/pages/registration/registration'
+import { Gallery } from './components/pages/gallery/gallery'
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route path="/user" component={MyPage} exact/>
-      <Route path="/friends" component={Friends} exact/>
-      <Route path="/message" component={Message} exact/>
-      <Route path="/auth" component={AuthPage} exact/>
-      <Route path="/registration" component={Registration} exact/>
-      <Route path="/" component={() => <Redirect to="/auth"/>} />
+      <Route path="/user" component={MyPage} exact />
+      <Route path="/friends" component={Friends} exact />
+      <Route path="/message" component={Message} exact />
+      <Route path="/auth" component={AuthPage} exact />
+      <Route path="/registration" component={Registration} exact />
+      <Route path="/gallery" component={Gallery} exact />
+      {/*<Route path="/" component={() => <Redirect to="/auth"/>} />*/}
+      <Route path="/" component={() => <Redirect to="/user" />} />
     </Switch>
   )
 }
