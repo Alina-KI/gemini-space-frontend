9b71fed34476b9624065b7211d33a68fe349fb5c
Alina-KI <kraynova.alina.2002@mail.ru>
2021-09-06 21:20:17 +0300

    fixed gallery

--- src/components/pages/gallery/gallery.tsx
+++ src/components/pages/gallery/gallery.tsx
@@ -1,27 +1,94 @@
 import React from 'react'
 import s from './gallery.module.scss'
+import picture1 from '../../../images/1.jpg'
+import picture2 from '../../../images/18.jpg'
+import picture3 from   '../../../images/2.jpg'
+import picture4 from   '../../../images/11.jpg'
+import picture5 from   '../../../images/3.jpg'
+import picture6 from   '../../../images/10.jpg'
+import picture7 from   '../../../images/4.jpg'
+import picture8 from   '../../../images/9.jpg'
+import picture9 from   '../../../images/5.jpg'
+import picture10 from   '../../../images/8.jpg'
+import picture11 from   '../../../images/12.jpg'
+import picture12 from   '../../../images/15.jpg'
+import picture13 from   '../../../images/16.jpg'
+import picture14 from   '../../../images/17.jpg'
+import picture15 from   '../../../images/13.jpg'
+import picture16 from   '../../../images/14.jpg'
+import picture17 from   '../../../images/6.jpg'
+import picture18 from   '../../../images/7.jpg'
+
 
 export const Gallery = () => {
-  // const images = ['http://localhost:300/images/1.jpg','../../../images/18.jpg',
-  //   '../../../images/2.jpg','../../../images/11.jpg',
-  //   '../../../images/3.jpg','../../../images/10.jpg',
-  //   '../../../images/4.jpg','../../../images/9.jpg',
-  //   '../../../images/5.jpg','../../../images/8.jpg',
-  //   '../../../images/12.jpg','../../../images/15.jpg',
-  //   '../../../images/16.jpg','../../../images/17.jpg',
-  //   '../../../images/13.jpg','../../../images/14.jpg',
-  //   '../../../images/6.jpg','../../../images/7.jpg']
   return (
-    <div className={s.container}>
-      {/*{images.map(value => {*/}
-      {/*  console.log(value)*/}
-      {/*  return(*/}
-      {/*    <div className={s.content}>*/}
-      {/*      <div className={s.image} style={{ backgroundImage: `url(${value})` }}/>*/}
-      {/*      <div className={s.image}> </div>*/}
-      {/*    </div>*/}
-      {/*  )*/}
-      {/*})}*/}
+    <div>
+      <h1>Gallery</h1>
+      <div className={s.container}>
+        {/*{images.map(value => {*/}
+        {/*  console.log(value)*/}
+        {/*  return(*/}
+        {/*    <div className={s.content} key={value}>*/}
+        {/*      <div className={s.image} style={{ backgroundImage: `url(${value})` }}/>*/}
+        {/*      <div className={s.image}> </div>*/}
+        {/*    </div>*/}
+        {/*  )*/}
+        {/*})}*/}
+        <div className={s.content}>
+          <img className={s.image} src={picture1} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture2} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture3} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture4} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture5} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture6} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture7} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture8} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture9} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture10} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture11} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture12} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture13} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture14} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture15} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture16} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture17} alt="img" />
+        </div>
+        <div className={s.content}>
+          <img className={s.image} src={picture18} alt="img" />
+        </div>
+      </div>
     </div>
   )
 }
