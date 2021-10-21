58d6ddaab03e72a7e7b04ada89ec2c33c29c28ff
Alina-KI <kraynova.alina.2002@mail.ru>
2021-10-21 23:07:07 +0300

    fixed search input

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -45,22 +45,24 @@ export const Header = () => {
         </button>
         {isActive && <div onClick={() => setIsActive(false)} className={s.active_background} />}
         <div className={`${s.menu} ${isActive && s.active_menu}`}>
-          <input type="text" placeholder="Search..." className={s.search_navbar} />
+          <div className={s.navbar_search__container}>
+            <input className={s.navbar_search__input} type="text" placeholder="Search"/>
+          </div>
           <div className={s.navbar}>
             <Navbar setIsActive={setIsActive} />
           </div>
         </div>
-        {/*<div className={s.light_night}>*/}
-        {/*  <img className={s.img} src={sun} alt="sun" />*/}
-        {/*  <img className={s.img} src={moon} alt="moon" />*/}
-        {/*</div>*/}
         <div className={s.logo_name}>
           <NavLink to={`/user/${user?.login}`}><img className={s.logo} src={logo} alt="Logo" /></NavLink>
           <span className={s.header_name_project}><NavLink to={`/user/${user?.login}`}>{nameProject}</NavLink></span>
         </div>
         <div className={s.search}>
-          <input type="text" placeholder="Search..." className={s.header_search} />
+          <input type="text" className={s.searchTerm} placeholder="What are you looking for?"/>
+          <button type="submit" className={s.searchButton}>
+            <img src="data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E" alt="" />
+          </button>
         </div>
+
         {user === null ?
           <div className={s.login}><NavLink to="/auth">Login</NavLink></div>
           :
