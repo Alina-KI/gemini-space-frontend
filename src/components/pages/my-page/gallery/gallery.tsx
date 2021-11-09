15100463350a5a4e5bf947d6d4386388a80c1ccd
Alina-KI <kraynova.alina.2002@mail.ru>
2021-11-09 21:51:51 +0300

    fixed gallery

--- src/components/pages/my-page/gallery/gallery.tsx
+++ src/components/pages/my-page/gallery/gallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './gallery.module.scss'
 import { NavLink } from 'react-router-dom'
 import photo1 from '../../../../images/16.jpg'
@@ -7,14 +7,15 @@ import photo3 from '../../../../images/4.jpg'
 import photo4 from '../../../../images/17.jpg'
 
 export const Gallery = () => {
+  const [pathPhoto, setPathPhoto] = useState('')
 
   return (
     <div className={s.gallery}>
       <NavLink to="/gallery" className={s.whole_gallery}>Gallery</NavLink>
-      <NavLink to="/gallery/photo/:pk" className={s.gallery_photo} style={{ backgroundImage: `url("${photo1}")` }}/>
-      <NavLink to="/gallery/photo/:pk" className={s.gallery_photo} style={{ backgroundImage: `url("${photo2}")` }}/>
-      <NavLink to="/gallery/photo/:pk" className={s.gallery_photo} style={{ backgroundImage: `url("${photo3}")` }}/>
-      <NavLink to="/gallery/photo/:pk" className={s.gallery_photo} style={{ backgroundImage: `url("${photo4}")` }}/>
+      <button className={s.gallery_photo} style={{ backgroundImage: `url("${photo1}")` }}/>
+      <button className={s.gallery_photo} style={{ backgroundImage: `url("${photo2}")` }}/>
+      <button className={s.gallery_photo} style={{ backgroundImage: `url("${photo3}")` }}/>
+      <button className={s.gallery_photo} style={{ backgroundImage: `url("${photo4}")` }}/>
     </div>
   )
 }
