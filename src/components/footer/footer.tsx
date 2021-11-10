8fbf1988b13dad23f3bbc40fb1398210b1a4a689
Alina-KI <kraynova.alina.2002@mail.ru>
2021-11-10 17:43:10 +0300

    modify footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,47 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './footer.module.scss'
 import home from '../../images/menuIcon/home.svg'
 import person from '../../images/menuIcon/person.svg'
 import cell from '../../images/menuIcon/cell.svg'
 import chat from '../../images/menuIcon/chat.svg'
 import setting from '../../images/menuIcon/setting.svg'
+import homeActive from '../../images/menuIcon/homeActive.svg'
+import personActive from '../../images/menuIcon/personActive.svg'
+import cellActive from '../../images/menuIcon/cellActive.svg'
+import chatActive from '../../images/menuIcon/chatActive.svg'
+import settingActive from '../../images/menuIcon/settingActive.svg'
 
 export const Footer = () => {
+  const [activeLink, setActiveLink] = useState('Cell')
+  const [pos, setPos] = useState(10)
+
   return (
     <div className={s.footer}>
       <div className={s.containerFooter}>
         Footer
       </div>
       <div className={s.containerMenu}>
-        <div className={s.list}>
-          <a className={s.link} href="#">
-            <img className={s.icon} src={home} alt="Home" />
-            Home
+        <div className={`${ s.list } ${activeLink === 'Home' ? `${s.active}`: ''}`}>
+          <a className={s.link} href="#" onClick={() => {
+            setActiveLink('Home')
+            setPos(10)
+          }}>
+            <img className={s.icon} src={ activeLink === 'Home' ? homeActive: home} alt="Home" />
+            <span className={s.text}>Home</span>
           </a>
         </div>
-        <div className={s.list}>
-          <a className={s.link} href="#">
-            <img className={s.icon} src={person} alt="Person" />
-            Person
+        <div className={`${ s.list } ${activeLink === 'Profile' ? `${s.active}`: ''}`}>
+          <a className={s.link} href="#" onClick={() => {
+            setActiveLink('Profile')
+            setPos(30)
+          }}>
+            <img className={s.icon} src={ activeLink === 'Profile' ? personActive: person} alt="Profile" />
+            <span className={s.text}>Profile</span>
           </a>
         </div>
-        <div className={s.list}>
-          <a className={`${s.link} ${s.cell}`} href="#">
-            <img className={s.icon} src={cell} alt="Cell" />
+        <div className={`${ s.list } ${activeLink === 'Cell' ? `${s.active}`: ''}`}>
+          <a className={s.link} href="#" onClick={() => {
+            setActiveLink('Cell')
+            setPos(50)
+          }}>
+            <img className={s.icon} src={ activeLink === 'Cell' ? cellActive: cell} alt="Cell" />
+            <span className={s.text}>Cell</span>
           </a>
         </div>
-        <div className={s.list}>
-          <a className={s.link} href="#">
-            <img className={s.icon} src={chat} alt="Chat" />
-            Chat
+        <div className={`${ s.list } ${activeLink === 'Chat' ? `${s.active}`: ''}`}>
+          <a className={s.link} href="#" onClick={() => {
+            setActiveLink('Chat')
+            setPos(70)
+          }}>
+            <img className={s.icon} src={ activeLink === 'Chat' ? chatActive: chat} alt="Chat" />
+            <span className={s.text}>Chat</span>
           </a>
         </div>
-        <div className={s.list}>
-          <a className={s.link} href="#">
-            <img className={s.icon} src={setting} alt="Setting" />
-            Setting
+        <div className={`${ s.list } ${activeLink === 'Setting' ? `${s.active}`: ''}`}>
+          <a className={s.link} href="#" onClick={() => {
+            setActiveLink('Setting')
+            setPos(90)
+          }}>
+            <img className={s.icon} src={ activeLink === 'Setting' ? settingActive: setting} alt="Setting" />
+            <span className={s.text}>Setting</span>
           </a>
         </div>
+        <div className={s.slider} style={{ left: `${pos}%` }}> </div>
       </div>
     </div>
   )
