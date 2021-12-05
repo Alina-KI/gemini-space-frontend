afabc6ffc9ada79fde4038819bda1f7416a2e7b2
Alina-KI <kraynova.alina.2002@mail.ru>
2021-12-05 23:57:04 +0300

    modify footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -20,56 +20,56 @@ export const Footer = () => {
   return (
     <div className={s.footer}>
       <div className={s.containerFooter}>
-        Footer
+        <div className={s.text}>Footer</div>
       </div>
       {isLoadingPage &&
       <div className={s.containerMenu}>
-        <div className={`${ s.list } ${activeLink === 'Home' ? `${s.active}`: ''}`}>
+        <div className={`${s.list} ${activeLink === 'Home' ? `${s.active}` : ''}`}>
           <a className={s.link} href="#" onClick={() => {
             setActiveLink('Home')
             setPos(10)
           }}>
-            <img className={s.icon} src={ activeLink === 'Home' ? homeActive: home} alt="Home" />
+            <img className={s.icon} src={activeLink === 'Home' ? homeActive : home} alt="Home" />
             <span className={s.text}>Home</span>
           </a>
         </div>
-        <div className={`${ s.list } ${activeLink === 'Profile' ? `${s.active}`: ''}`}>
+        <div className={`${s.list} ${activeLink === 'Profile' ? `${s.active}` : ''}`}>
           <a className={s.link} href="#" onClick={() => {
             setActiveLink('Profile')
             setPos(30)
           }}>
-            <img className={s.icon} src={ activeLink === 'Profile' ? personActive: person} alt="Profile" />
+            <img className={s.icon} src={activeLink === 'Profile' ? personActive : person} alt="Profile" />
             <span className={s.text}>Profile</span>
           </a>
         </div>
-        <div className={`${ s.list } ${activeLink === 'Cell' ? `${s.active}`: ''}`}>
+        <div className={`${s.list} ${activeLink === 'Cell' ? `${s.active}` : ''}`}>
           <a className={s.link} href="#" onClick={() => {
             setActiveLink('Cell')
             setPos(50)
           }}>
-            <img className={s.icon} src={ activeLink === 'Cell' ? cellActive: cell} alt="Cell" />
+            <img className={s.icon} src={activeLink === 'Cell' ? cellActive : cell} alt="Cell" />
             <span className={s.text}>Cell</span>
           </a>
         </div>
-        <div className={`${ s.list } ${activeLink === 'Chat' ? `${s.active}`: ''}`}>
+        <div className={`${s.list} ${activeLink === 'Chat' ? `${s.active}` : ''}`}>
           <a className={s.link} href="#" onClick={() => {
             setActiveLink('Chat')
             setPos(70)
           }}>
-            <img className={s.icon} src={ activeLink === 'Chat' ? chatActive: chat} alt="Chat" />
+            <img className={s.icon} src={activeLink === 'Chat' ? chatActive : chat} alt="Chat" />
             <span className={s.text}>Chat</span>
           </a>
         </div>
-        <div className={`${ s.list } ${activeLink === 'Setting' ? `${s.active}`: ''}`}>
+        <div className={`${s.list} ${activeLink === 'Setting' ? `${s.active}` : ''}`}>
           <a className={s.link} href="#" onClick={() => {
             setActiveLink('Setting')
             setPos(90)
           }}>
-            <img className={s.icon} src={ activeLink === 'Setting' ? settingActive: setting} alt="Setting" />
+            <img className={s.icon} src={activeLink === 'Setting' ? settingActive : setting} alt="Setting" />
             <span className={s.text}>Setting</span>
           </a>
         </div>
-        <div className={s.slider} style={{ left: `${pos}%` }}> </div>
+        <div className={s.slider} style={{ left: `${pos}%` }}/>
       </div>
       }
     </div>
