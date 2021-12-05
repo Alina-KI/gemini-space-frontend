594251be1f7cdcbf72b85b4a9a4275db7e07d7aa
Alina-KI <kraynova.alina.2002@mail.ru>
2021-12-06 00:48:10 +0300

    scaffolded message/dialogue

--- src/components/pages/message/message.tsx
+++ src/components/pages/message/message.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { io } from 'socket.io-client'
 import s from './message.module.scss'
-import { ErrorDisplay } from '../../shared/error-display/error-display'
 
 export const Message = () => {
+  const socket = io('http://localhost:5000')
+  socket.on('connect', function() {
+    console.log('Connected')
+    socket.emit('events', { test: 'test' })
+    socket.emit('identity', 0, (response: any) =>
+      console.log('Identity:', response)
+    )
+  })
+  socket.on('events', function(data) {
+    console.log('event', data)
+  })
+  socket.on('exception', function(data) {
+    console.log('event', data)
+  })
+  socket.on('disconnect', function() {
+    console.log('Disconnected')
+  })
   return (
     <div className={s.container}>
-      {/*<ErrorDisplay code={502}*/}
-      {/*  message={'Bad request'}/>*/}
       <div className={s.messages}>
         <div className={s.message}>
           <div className={s.name}>
