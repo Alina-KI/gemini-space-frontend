a89198b2bd496e95e3dcae9eae004abb398a78b9
Alina-KI <kraynova.alina.2002@mail.ru>
2022-03-27 16:28:21 +0300

    added community

--- src/components/pages/community/community.tsx
+++ src/components/pages/community/community.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import s from './community.module.scss'
+import photo from '../../../images/11.jpg'
+import { NavLink } from 'react-router-dom'
 
 export const Community = () => {
   return (
-    <div>
-      
+    <div className={s.container}>
+      <div className={s.card}>
+        <img className={s.img} src={photo} alt="photo" />
+        <div className={s.info}>
+          <NavLink to="/" className={s.title}>Req Still</NavLink>
+          <span>Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+          <button className={s.addFriend}>Add friends</button>
+        </div>
+      </div>
+      <div className={s.card}>
+        <img className={s.img} src={photo} alt="photo" />
+        <div className={s.info}>
+          <NavLink to="/" className={s.title}>Req Still</NavLink>
+          <span>Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex nisi, porro. Aperiam beatae doloremque eligendi facilis in iure non suscipit vitae! Aperiam atque cupiditate eius, explicabo inventore modi. Fugiat, veritatis.</span>
+          <button className={s.addFriend}>Add friends</button>
+        </div>
+      </div>
+      <div className={s.card}>
+        <img className={s.img} src={photo} alt="photo" />
+        <div className={s.info}>
+          <NavLink to="/" className={s.title}>Req Still</NavLink>
+          <span>Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eius pariatur quam, suscipit tempora </span>
+          <button className={s.addFriend}>Add friends</button>
+        </div>
+      </div>
     </div>
   )
 }
