5f346495d41bc2be1be1cf9b028eed3fdc2e1598
Alina-KI <kraynova.alina.2002@mail.ru>
2022-05-09 01:01:28 +0300

    fixed dialogs

--- src/components/pages/dialogs/dialogs.tsx
+++ src/components/pages/dialogs/dialogs.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import s from './dialogs.module.scss'
+import photo from '../../../images/2.jpg'
 import { observer } from 'mobx-react-lite'
 import { dialogsStore } from '../../../store/dialogs-store'
 import { NavLink } from 'react-router-dom'
-import { toJS } from 'mobx'
 
 export const Dialogs = observer(() => {
   return (
     <div>
-      {dialogsStore.dialogs.map(dialog => <NavLink to={`/dialogs/${dialog._id}`} key={dialog._id}>
-        <h5>{dialog.nameTalk}</h5>
+      {dialogsStore.dialogs.map(dialog => <NavLink className={s.dialog} to={`/dialogs/${dialog._id}`} key={dialog._id}>
+        <img className={s.photoDialog} src={photo} alt="photoDialog" />
+        <div className={s.dataDialog}>
+          <h4 className={s.nameTalk}>{dialog.nameTalk}</h4>
+          <p className={s.dataMessage}>{dialog._id}</p>
+          <p className={s.lastMessage}>{dialog._id}</p>
+        </div>
       </NavLink>)}
     </div>
   )
