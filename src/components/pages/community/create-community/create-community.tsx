af76d4f7f2f2456957263508882b3e2a80385b5b
Alina-KI <kraynova.alina.2002@mail.ru>
2022-05-30 02:18:22 +0300

    added create-community

---
+++ src/components/pages/community/create-community/create-community.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './create-community.module.scss'
+
+export const CreateCommunity = () => {
+  return (
+    <div className={s.container}>
+
+    </div>
+  )
+}
